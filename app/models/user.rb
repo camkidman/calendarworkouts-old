a40ef4f6a170809ad494483266b22af7ceb25c3f
Cameron K <cameronjkidman@gmail.com>
2013-07-20 19:27:31 -0600

    destroy association user to pd

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   attr_accessible :birthday, :birthmonth, :birthyear, :city, :country, :firstname, :lastname, :state, :username
-  has_one :personal_data
+  has_one :personal_data, dependent: :destroy
 end
