f91e1e9f157874201a9bff84bec1b394374699c0
Cameron K <cameronjkidman@gmail.com>
2013-07-20 18:13:58 -0600

    Initial commit. Rails 3.2.13

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Calendarworkouts::Application.config.secret_token = 'a1d6e2c2c20be28a20d7233387ff1b9d03b3822ed5fcb7fd3ea03e56584fe67f21dbf4d96a6a4b56fe134e1e341b8e556c029b9a689101cb8d0593c4c069fb24'
