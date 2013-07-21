5dffc6420193f951167711504913e3de058b9a43
Cameron K <cameronjkidman@gmail.com>
2013-07-20 18:39:45 -0600

    Configured devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Calendarworkouts::Application.routes.draw do
   resources :users
 
-
+  root :to => "home#index" 
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
