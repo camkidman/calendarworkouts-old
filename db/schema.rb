9e9c84b6b0b8acc836e662b5b86a74c31612ff7b
Cameron K <cameronjkidman@gmail.com>
2013-07-20 20:30:45 -0600

    Migrated workouts and body parts tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130721012305) do
+ActiveRecord::Schema.define(:version => 20130721021351) do
+
+  create_table "body_parts", :force => true do |t|
+    t.string   "name"
+    t.string   "muscle_group"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
 
   create_table "exercises", :force => true do |t|
     t.string   "name"
@@ -66,5 +73,13 @@ ActiveRecord::Schema.define(:version => 20130721012305) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+
+  create_table "workouts", :force => true do |t|
+    t.string   "name"
+    t.integer  "exercise_id"
+    t.integer  "body_part_id"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
 
 end
