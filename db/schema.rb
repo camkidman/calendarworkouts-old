d6038fcf1b972efd2e091f60e23e04078ab7049a
Cameron K <cameronjkidman@gmail.com>
2013-07-22 17:00:36 -0600

    Migrated video_url for exercises

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130722225619) do
+ActiveRecord::Schema.define(:version => 20130722225937) do
 
   create_table "body_parts", :force => true do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20130722225619) do
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
     t.string   "muscle_group"
+    t.string   "video_url"
   end
 
   create_table "personal_data", :force => true do |t|
